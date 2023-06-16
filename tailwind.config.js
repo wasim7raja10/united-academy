99ae4421cb9db0318fb4e7b7b2b4a2af19756aeb
Wasim Raja <wasim7raja10@gmail.com>
2023-06-16 20:34:34 +0530

    color in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,13 +6,14 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
+    colors: {
+      'dark': '#191A1A',
+      'light': '#FFFFFF',
+      'primary': '#0A43A0',
+      'secondary': '#FFA515',
+      'error': '#EE5521',
+      'success': '#477F42',
+    }
   },
   plugins: [],
 }
