a822e77d51df03d48cd31ce8d1a81aff8da19bbd
Wasim Raja <wasim7raja10@gmail.com>
2023-06-16 21:22:18 +0530

    footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,6 @@ import Image from "next/image";
 export default function Home() {
 	return (
 		<main className=''>
-			<nav>
-				{/* phone number */}
-				<p className='text-center h-50 uppercase '>
-					CALL: 9735904503 / 9593488288
-				</p>
-			</nav>
 		</main>
 	);
 }
