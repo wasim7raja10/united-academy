5800149cb6882f37d530b10b3ee5155c292cd41e
Wasim Raja <wasim7raja10@gmail.com>
2023-06-17 11:07:02 +0530

    menu

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
-import Image from "next/image";
+import Footer from "@/components/Footer";
 import "./globals.css";
 import { Inter } from "next/font/google";
-import { MdEmail, MdPhone, MdLocationOn } from "react-icons/md";
+import Navbar from "@/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,55 +18,9 @@ export default function RootLayout({
 	return (
 		<html lang='en'>
 			<body className={inter.className}>
-				<nav>NAVBAR</nav>
-
+				<Navbar />
 				{children}
-
-				<footer className='text-neutral'>
-					<div className='relative h-60 w-full'>
-						{/* background image */}
-						<Image
-							className='-z-20'
-							fill
-							src='/footer-img.png'
-							alt='background image'
-						/>
-						<div className='absolute top-0 bottom-0 left-0 right-0 bg-primary bg-opacity-80 -z-10'></div>
-						<div className='z-10 p-5 flex flex-col justify-center h-full'>
-							<div className='space-y-6'>
-								<div className='flex items-center gap-4'>
-									<MdLocationOn className=' text-2xl text-secondary' />
-									<div className=' text-xl tracking-wide'>
-										<p>United Academy School</p>
-										<p className='text-base'>Motiganj, Samsi, Malda</p>
-									</div>
-								</div>
-								<div className='flex items-center gap-4'>
-									<MdPhone className=' text-2xl text-secondary' />
-									<a
-										className=' text-xl tracking-wide'
-										href='tel:+919735904503'
-									>
-										9735904503
-									</a>
-								</div>
-								<div className='flex items-center gap-4'>
-									<MdEmail className=' text-2xl text-secondary' />
-									<a
-										className=' text-xl tracking-wide'
-										href='mailto: unitedacademy@gmail.com'
-									>
-										unitedacademy@gmail.com
-									</a>
-								</div>
-							</div>
-						</div>
-					</div>
-					{/* copyright */}
-					<p className='text-dark bg-secondary py-4 text-center'>
-						© 2023 United Academy, Motiganj, Samsi. <br /> All rights reserved.
-					</p>
-				</footer>
+				<Footer />
 			</body>
 		</html>
 	);
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { useState } from "react";
+import { MdClose, MdMenu } from "react-icons/md";
+
+export default function Navbar() {
+	const [isMenuOpen, setIsMenuOpen] = useState(false);
+	function onMenuButtonClick() {
+		setIsMenuOpen((prev) => !prev);
+	}
+	return (
+		<nav className='px-5 py-4 sticky top-0'>
+			<div className='flex justify-between items-center'>
+				<div>LOGO</div>
+				<button
+					type='button'
+					onClick={onMenuButtonClick}
+					className='p-2 z-50 right-3 text-dark text-2xl'
+				>
+					{!isMenuOpen && <MdMenu />}
+					{isMenuOpen && <MdClose />}
+				</button>
+			</div>
+			{isMenuOpen && (
+				<>
+					<div
+						onClick={onMenuButtonClick}
+						className='fixed bg-dark bg-opacity-70 top-0 bottom-0 right-0 left-0 z-10'
+					></div>
+					<div className='fixed bg-light top-0 bottom-0 right-0 left-24 z-50'></div>
+				</>
+			)}
+		</nav>
+	);
+}
