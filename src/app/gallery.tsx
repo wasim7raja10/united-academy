e9262997495e20460f01798f0af72b2d439d735b
Wasim Raja <wasim7raja10@gmail.com>
2023-09-09 23:26:54 +0530

    ok

--- src/app/gallery.tsx
+++
@@ -1,6 +0,0 @@
-export default function Gallery() {
-  return (
-    <main className=''>
-    </main>
-  )
-}
--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Notice from "@/components/Notice";
-
-export default function Home() {
-	return (
-		<main className=''>
-			<Notice />
-		</main>
-	);
-}
