cd158c7c7fce1b68603ef859fb2b19b4c188ba54
officialsaurabh <hmsaurabhkr@gmail.com>
2023-10-10 00:56:49 +0530

    add about page

--- src/shared/data/dummy/teachers.ts
+++ src/shared/data/dummy/teachers.ts
@@ -1,37 +1,37 @@
 export const teachers = [
 	{
 		id: 1,
-		imageSrc: "/user1.jpg",
+		imageSrc: "https://github.com/shadcn.png",
 		name: "John Doe",
 		title: "Principle",
 	},
 	{
 		id: 2,
-		imageSrc: "/user1.jpg",
+		imageSrc: "https://github.com/shadcn.png",
 		name: "John Doe",
 		title: "Principle",
 	},
 	{
 		id: 3,
-		imageSrc: "/user1.jpg",
+		imageSrc: "https://github.com/shadcn.png",
 		name: "John Doe",
 		title: "Principle",
 	},
 	{
 		id: 4,
-		imageSrc: "/user1.jpg",
+		imageSrc: "https://github.com/shadcn.png",
 		name: "John Doe",
 		title: "Principle",
 	},
 	{
 		id: 5,
-		imageSrc: "/user1.jpg",
+		imageSrc: "https://github.com/shadcn.png",
 		name: "John Doe",
 		title: "Principle",
 	},
 	{
 		id: 6,
-		imageSrc: "/user1.jpg",
+		imageSrc: "https://github.com/shadcn.png",
 		name: "John Doe",
 		title: "Principle",
 	},
