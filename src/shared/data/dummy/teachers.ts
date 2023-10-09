cf29ada4204fce374b48f25fbe54bd727aad892e
Wasim Raja <wasim7raja10@gmail.com>
2023-10-09 21:07:32 +0530

    fix carousel

---
+++ src/shared/data/dummy/teachers.ts
@@ -0,0 +1,38 @@
+export const teachers = [
+	{
+		id: 1,
+		imageSrc: "/user1.jpg",
+		name: "John Doe",
+		title: "Principle",
+	},
+	{
+		id: 2,
+		imageSrc: "/user1.jpg",
+		name: "John Doe",
+		title: "Principle",
+	},
+	{
+		id: 3,
+		imageSrc: "/user1.jpg",
+		name: "John Doe",
+		title: "Principle",
+	},
+	{
+		id: 4,
+		imageSrc: "/user1.jpg",
+		name: "John Doe",
+		title: "Principle",
+	},
+	{
+		id: 5,
+		imageSrc: "/user1.jpg",
+		name: "John Doe",
+		title: "Principle",
+	},
+	{
+		id: 6,
+		imageSrc: "/user1.jpg",
+		name: "John Doe",
+		title: "Principle",
+	},
+];
