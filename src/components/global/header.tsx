dd8164f3dde9214a88b658f6b7ebc79ec6291fa1
Wasim Raja <wasim7raja10@gmail.com>
2023-11-30 14:29:38 +0530

    added 1 package, and audited 1 package in 1s

--- src/components/global/header.tsx
+++ src/components/global/header.tsx
@@ -24,7 +24,7 @@ import {
 export const Header = () => {
   return (
     <header className="sticky top-0 z-50 bg-white shadow-sm">
-      <div className="container py-4 flex items-center justify-between">
+      <div className="container py-2 flex items-center justify-between">
         <div className="flex items-center gap-4">
           <img
             src={"/favicon.svg"}
