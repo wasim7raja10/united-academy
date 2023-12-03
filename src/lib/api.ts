e3c328ead83340e23f284f0d57018862ffed27f8
Wasim Raja <wasim7raja10@gmail.com>
2023-12-03 16:25:48 +0530

    Refactor API and syllabus code

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,10 +1,21 @@
 import { useSanityClient } from "@sanity/astro";
 import type { SanityAsset } from "@sanity/image-url/lib/types/types";
 
-export async function getHomepageBanners(): Promise<any> {
-	const query = `*[_type == "images" && slug.current == 'homepage-banner'][0].images[]{
+export async function getHomepageBanners(): Promise<SanityAsset> {
+  const query = `*[_type == "images" && slug.current == 'homepage-banner'][0].images[]{
     asset
   }`;
-	const images = await useSanityClient().fetch(query);
-	return images;
+  const images = await useSanityClient().fetch<SanityAsset>(query);
+  return images;
+}
+
+export async function getSyllabus(): Promise<any> {
+  const query = `*[_type == "pdf" && slug.current == 'syllabus'][0].pdf[] {
+		asset->{
+			url,
+			originalFilename
+		}
+	}`;
+  const syllabus = await useSanityClient().fetch<any>(query);
+  return syllabus;
 }
