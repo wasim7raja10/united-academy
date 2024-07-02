88ed42510c8668c274c8e8e429fbe56c0c396bce
Wasim Raja <wasim7raja10@gmail.com>
2024-07-02 22:45:07 +0530

    chore: Add "embla-carousel-autoplay" dependency and update homepage carousel

--- src/components/global/homepage-carousel.tsx
+++ src/components/global/homepage-carousel.tsx
@@ -1,16 +1,19 @@
 import { urlForImageClient } from "@/lib/utils";
 import type { SanityAsset } from "@sanity/image-url/lib/types/types";
 import { Carousel, CarouselContent, CarouselItem } from "../ui/carousel";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { getHomepageBanners } from "@/lib/api";
+import Autoplay from "embla-carousel-autoplay";
 
 export default function HomepageCarousel() {
   const [images, setImages] = useState<SanityAsset>([]);
 
+  const plugin = useRef(Autoplay({ delay: 2000 }));
+
   useEffect(() => {
     getHomepageBanners().then((images) => setImages(images));
   }, []);
-  
+
   return (
     <div className=" max-w-[1800px] mx-auto">
       <section
@@ -18,7 +21,7 @@ export default function HomepageCarousel() {
         id="carousel"
         aria-label="Homepage Image Carousel"
       >
-        <Carousel>
+        <Carousel plugins={[plugin.current]}>
           <CarouselContent>
             {images.map((it: any) => (
               <CarouselItem className="aspect-[343/90]">
@@ -26,6 +29,8 @@ export default function HomepageCarousel() {
                   src={urlForImageClient(it.asset).url()}
                   loading="lazy"
                   alt="Homepage Banner Image"
+                  height={600}
+                  width={2000}
                 />
               </CarouselItem>
             ))}
